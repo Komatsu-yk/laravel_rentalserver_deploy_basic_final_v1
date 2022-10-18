f96c79424d61f67a7cd5feef12d67ea5cbd2c8cd
@student_55232 <pyk1017574@yahoo.co.jp>
2022-10-18 12:16:53 +0000

    Revert "Merge branch 'issue/#1' into 'master'"
    This reverts merge request !2

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -6,8 +6,6 @@ use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
-use Illuminate\Http\Request;
-
 class LoginController extends Controller
 {
     /*
@@ -28,7 +26,7 @@ class LoginController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/posts';
+    protected $redirectTo = RouteServiceProvider::HOME;
 
     /**
      * Create a new controller instance.
@@ -38,10 +36,5 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
-    }
-    
-    protected function loggedOut(Request $request)
-    {
-        return redirect(route('login'));
     }
 }
